b8b1fc45df13183be9eb4c34d01e02de1add3561
TarasFydriaDelphi <Taras.Fydria@udelphi.com>
2024-03-29 23:34:00 +0200

    change .env server path

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,17 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { SeedsModule } from './seeds/seeds.module';
-import { DatabaseModule } from './database/database.module';
 import { DatabaseService } from './database/database.service';
+import * as path from 'path';
 
 @Module({
   imports: [
     ProductsModule,
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: path.resolve(__dirname, '..', '.env'),
     }),
     TypeOrmModule.forRootAsync({ useClass: DatabaseService }),
   ],
