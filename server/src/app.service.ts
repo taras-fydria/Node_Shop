68eb803398d6c80673840a8b04530e423976da89
TarasFydriaDelphi <Taras.Fydria@udelphi.com>
2024-08-08 21:01:13 +0300

    test commit

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Deployed by github actions';
+    return 'Hello World! Deployed by github actions 123';
   }
 }
